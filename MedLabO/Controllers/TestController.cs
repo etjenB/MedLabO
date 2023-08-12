3848fa86e0986e95a53ece68e7d55b8463fc078b
Etjen Buljubašić <buljubasicetjen4@gmail.com>
2023-08-12 19:25:46 +0200

    fix ValidateIssuer and ValidateAudience for testing

--- MedLabO/Controllers/TestController.cs
+++ MedLabO/Controllers/TestController.cs
@@ -1,7 +1,9 @@
 ﻿using MedLabO.Models;
+using MedLabO.Models.Constants;
 using MedLabO.Models.Requests;
 using MedLabO.Models.SearchObjects;
 using MedLabO.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MedLabO.Controllers
@@ -14,6 +16,7 @@ namespace MedLabO.Controllers
         }
 
         [HttpPut("{Id}/ChangeName")]
+        [Authorize(Roles = RoleNames.Pacijent + "," + RoleNames.Administrator)]
         public virtual async Task<Test> ChangeName(Guid Id, [FromBody] string newName)
         {
             return await (_service as ITestService).ChangeName(Id, newName);
