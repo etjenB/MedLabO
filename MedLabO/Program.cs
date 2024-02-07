55d91f5efbdd0ccf9154d1dd867147f6ce71fb5d
Etjen Buljubašić <buljubasicetjen4@gmail.com>
2024-02-07 20:53:31 +0100

    swag

--- MedLabO/Program.cs
+++ MedLabO/Program.cs
@@ -140,14 +140,11 @@ builder.Services.AddAuthentication(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
-//}
-
-app.UseSwagger();
-app.UseSwaggerUI();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
